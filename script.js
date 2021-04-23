70b5c098a66e5c7b36e60456093ca1dde0ff47d4
devikrishnan <devikrishnan67@gmail.com>
2021-04-23 23:34:20 +0200

    dd

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 const questions = [
     {
-        question: "Which dinosaur is the biggest carnivore ever found?",
-        optionA: "Spinosaurus",
-        optionB: "Tyrannosaurus",
-        optionC: "Velociraptor",
-        optionD: "Triceratops",
+        question: "How many days makes a week ?",
+        optionA: "10 days",
+        optionB: "14 days",
+        optionC: "5 days",
+        optionD: "7 days",
         correctOption: "optionD"
     },
 
     {
-        question: "What is the name of the dinosaur that has three horns on the head?",
-        optionA: "Triceratops",
-        optionB: "Tyrannosaurus",
-        optionC: "Velociraptor",
-        optionD: "Megalosaurus",
+        question: "How many players are allowed on a soccer pitch ?",
+        optionA: "10 players",
+        optionB: "11 players",
+        optionC: "9 players",
+        optionD: "12 players",
         correctOption: "optionB"
     },
 
     {
-        question: "Which dinosaur is the biggest carnivore ever found?",
-        optionA: "Spinosaurus",
-        optionB: "Tyrannosaurus",
-        optionC: "Velociraptor",
-        optionD: "Stegosaurus",
+        question: "Who was the first President of USA ?",
+        optionA: "Donald Trump",
+        optionB: "Barack Obama",
+        optionC: "Abraham Lincoln",
+        optionD: "George Washington",
         correctOption: "optionD"
     },
 
     {
-        question: "Name the dinosaur that can fly ?",
-        optionA: "Pterodactyl",
-        optionB: "Stegosaurus",
-        optionC: "Mosasaurus",
-        optionD: "Mosasaurus",
+        question: "30 days has ______ ?",
+        optionA: "January",
+        optionB: "December",
+        optionC: "June",
+        optionD: "August",
         correctOption: "optionC"
     },
 
     {
-        question: "Name the dinosaur that is an aquatic dinosaur",
-        optionA: "Mosasaurus",
-        optionB: "Velociraptor",
-        optionC: "Triceratops",
-        optionD: "Iguanadon",
+        question: "How manay hours can be found in a day ?",
+        optionA: "30 hours",
+        optionB: "38 hours",
+        optionC: "48 hours",
+        optionD: "24 hours",
         correctOption: "optionD"
     },
 
